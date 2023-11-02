78161ad32077ed5eddb185ac6be8475285dbef61
Nahuel-206 <escuderonahuel36@gmail.com>
2023-11-02 20:58:45 -0300

    Prueba main

--- src/proyectofinalgrupo9/ProyectoFinalGrupo9.java
+++ src/proyectofinalgrupo9/ProyectoFinalGrupo9.java
@@ -1,10 +1,12 @@
 
 package proyectofinalgrupo9;
 
-import ProyectoFinalGrupo9.AccesoADATOS.Conexion;
-import ProyectoFinalGrupo9.ClasesEntidades.Bombero;
+import proyectofinalgrupo9.AccesoADATOS.Conexion;
+import proyectofinalgrupo9.ClasesEntidades.Bombero;
 import java.sql.Connection;
 import java.time.LocalDate;
+import java.time.Month;
+import proyectofinalgrupo9.AccesoADATOS.BomberoData;
 
 
 public class ProyectoFinalGrupo9 {
@@ -17,6 +19,10 @@ public class ProyectoFinalGrupo9 {
      
        /*-----------------------------------------------*/
        
+       Bombero jose = new Bombero("12345678","Luna jose", LocalDate.of(1980, 4, 25), "35447686", 0);
+       
+       BomberoData bombero = new BomberoData();
+       bombero.guardarBombero(jose);
        
     }
   
